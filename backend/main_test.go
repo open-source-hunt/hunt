083de03d9b18d64b6c83b0621615fe5815e50bd7
John Mayou <john@johnmayou.com>
2025-01-16 23:28:18 -0600

    return json from test endpoint

--- backend/main_test.go
+++ backend/main_test.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
-	assert.Equal(t, 1, 1)
+func TestWelcomeHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/api/welcome", nil)
+	rr := httptest.NewRecorder()
+
+	welcomeHandler(rr, req)
+
+	var message WelcomeMessage
+	assert.NoError(t, json.NewDecoder(rr.Body).Decode(&message))
+	assert.Equal(t, "Hello, World!", message.Message)
+	assert.Equal(t, http.StatusOK, rr.Code)
 }
